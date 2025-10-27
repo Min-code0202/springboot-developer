b38655907afb0592468be756f8628cfba4bdb482
Min-code0202 <jjm001115@gmail.com>
2025-10-27 22:43:52 +0900

    feat: 블로그 화면 구성

---
+++ src/main/java/com/jurumin/springboot_developer/dto/ArticleListViewResponse.java
@@ -0,0 +1,17 @@
+package com.jurumin.springboot_developer.dto;
+
+import com.jurumin.springboot_developer.domain.Article;
+import lombok.Getter;
+
+@Getter
+public class ArticleListViewResponse {
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
