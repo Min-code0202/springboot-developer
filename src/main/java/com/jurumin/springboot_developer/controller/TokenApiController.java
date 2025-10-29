684ff504fee40435fa594dfe62c024563d0ce58e
Min-code0202 <jjm001115@gmail.com>
2025-10-29 16:36:56 +0900

    feat: jwt 로그인/로그아웃 만들기

---
+++ src/main/java/com/jurumin/springboot_developer/controller/TokenApiController.java
@@ -0,0 +1,25 @@
+package com.jurumin.springboot_developer.controller;
+
+import com.jurumin.springboot_developer.dto.CreateAccessTokenRequest;
+import com.jurumin.springboot_developer.dto.CreateAccessTokenResponse;
+import com.jurumin.springboot_developer.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+
+    private final TokenService tokenService;
+
+    @PostMapping("/api/token")
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken(@RequestBody CreateAccessTokenRequest request) {
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new CreateAccessTokenResponse(newAccessToken));
+    }
+}
---
+++ src/main/java/com/jurumin/springboot_developer/service/RefreshTokenService.java
@@ -0,0 +1,18 @@
+package com.jurumin.springboot_developer.service;
+
+import com.jurumin.springboot_developer.domain.RefreshToken;
+import com.jurumin.springboot_developer.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+}
